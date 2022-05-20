04423a7a5c0e84442a2bf6e77b6a3319b58f71a1
Gitea <gitea@fake.local>
2022-05-20 21:47:27 +0800

    support random generation

--- rscrypt.go
+++ rscrypt.go
@@ -13,7 +13,9 @@ import (
 	"encoding/json"
 	"encoding/pem"
 	"errors"
+	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto/ecies"
 )
@@ -291,3 +293,37 @@ func generateKey(key []byte) (genKey []byte) {
 }
 
 // =================== ECB ======================
+
+// =================== random ===================
+func GetRandomInt(max *big.Int) (int, error) {
+	if max == nil {
+		seed := "0123456789"
+		alphanum := seed + fmt.Sprintf("%v", time.Now().UnixNano())
+		max = big.NewInt(int64(len(alphanum)))
+	}
+	vrand, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return 0, err
+	}
+	return int(vrand.Int64()), nil
+}
+
+func GetRandom(n int, isNO bool) string {
+	seed := "0123456789"
+	if !isNO {
+		seed = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	}
+	alphanum := seed + fmt.Sprintf("%v", time.Now().UnixNano())
+	buffer := make([]byte, n)
+	max := big.NewInt(int64(len(alphanum)))
+
+	for i := 0; i < n; i++ {
+		index, err := GetRandomInt(max)
+		if err != nil {
+			return ""
+		}
+
+		buffer[i] = alphanum[index]
+	}
+	return string(buffer)
+}
